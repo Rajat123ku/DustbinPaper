14ce97d8700cdc805ada32c189c9025c877b49d8
Rajat123ku <76685895+Rajat123ku@users.noreply.github.com>
2021-02-28 14:33:36 +0530

    Add files via upload

---
+++ dustbin.js
@@ -0,0 +1,9 @@
+class dustbin extends BaseClass {
+	constructor(x,y){
+    super(x, y, 150, 150);
+	this.image=loadImage("dustbingreen.png");
+	
+	}	
+	
+
+}
---
+++ paper.js
@@ -0,0 +1,10 @@
+class Paper extends BaseClass   {
+constructor(x,y,width,height) {
+super(100,650,width,height);
+this.image = loadImage("paper.png");
+
+
+}
+
+
+}
